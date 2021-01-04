6929924a22cab8ec64a2ac5fd1befbbc32cd2183
tridip25 <74949104+tridip25@users.noreply.github.com>
2021-01-04 10:38:29 +0530

    Add files via upload

---
+++ AnonymousExample/AnonymousExample/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnonymousExample
+{
+    class Customer
+    {
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public String EmailAddress { get; set; }
+    }
+}
---
+++ AnonymousExample/AnonymousExample/CustomerRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnonymousExample
+{
+    class CustomerRepository
+    {
+        public IList<Customer> Customers()
+        {
+            List<Customer> customerList = new List<Customer>();
+            customerList.Add(new Customer() {FirstName = "Brad" , LastName = "Potato" , EmailAddress = "a@abc.com"});
+            customerList.Add(new Customer() { FirstName = "Gotte", LastName = "Piyaj", EmailAddress = "piyaj@gmail.com" });
+            customerList.Add(new Customer() { FirstName = "Snoopy", LastName = "Hazarika", EmailAddress = "snop@yahoo.com" });
+            customerList.Add(new Customer() { FirstName = "Laura", LastName = "Paul", EmailAddress = "paul@abc.com" });
+            return customerList;
+        }
+    }
+}
